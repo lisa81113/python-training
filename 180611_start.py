31ae46edae48ce016d45e78195daf6921ed0298e
lisa81113 <40173687+lisa81113@users.noreply.github.com>
2018-06-11 22:25:39 +0800

    train test

---
+++ 180611_start.py
@@ -0,0 +1,28 @@
+# print("Hello 哈囉")
+# print(True)
+# #變數
+# input1=3 
+# input2="嗨"
+# print (input1)
+# print (input2)
+# input=1
+# print (input)
+# input=2 
+# print (input)
+# #運算符號
+# data=5//3#整數除法
+# data=6*3#6的3次方
+# print (data)
+#print("你"+"好")
+
+n1=input("Enter a Number：")
+n2=input("Enter a Number：")
+n1=int(n1)#將數字
+n2=int(n2)
+resiult=int(n1+n2)
+print(resiult)
+
+
+#讓使用者輸入5個數字，1.算總和；2.哪個數字最大
+#使用List列表處理
+
