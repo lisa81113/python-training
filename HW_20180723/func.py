0ecaeebce38b16bf1d59d058dd0be73710dd4e5e
Lisa Kao <40173687+lisa81113@users.noreply.github.com>
2018-07-16 22:22:04 +0800

    Add files via upload

---
+++ HW_20180723/func.py
@@ -0,0 +1,13 @@
+#作業1
+#1.1把99乘法表包裝成函式，可做成nxn 乘法
+#1.2把四則運算幾包裝成函式
+#1.3將以上函式包裝在你自己的模組和封包中,並且在主程式成功使用
+
+import lib.core as hw
+hw.list()
+hw.op(3,4,"+")
+
+#作業2
+#使用系統內建的模組radom產生1~100間的亂數
+import random 
+print (random.randrange(101))
